2a2b485365735e0432fb5c89419f657af20de478
jean <dnpxjean@hotmail.com>
2013-04-23 00:46:46 -0700

    aa

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MvcApplication1.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "jean well done.1";
+            ViewBag.Message = "jean well done.22";
 
             return View();
         }
