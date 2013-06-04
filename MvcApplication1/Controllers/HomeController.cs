bcd8be0750299801861dd995992fe47fe1f1409a
jinglocal <jinglocal@hotmail.com>
2013-06-04 11:31:59 +0800

    lastfail

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MvcApplication1.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "jean well done.0603fail";
+            ViewBag.Message = "jean well done.0604fail01";
 
             return View();
         }
