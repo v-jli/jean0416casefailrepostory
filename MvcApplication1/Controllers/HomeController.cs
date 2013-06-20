739e48a9b1c0cd01b0fd22fc71125f9f826167a5
jean <dnpxjean@hotmail.com>
2013-06-19 22:17:11 -0700

    1

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -6,11 +6,11 @@ using System.Web.Mvc;
 
 namespace MvcApplication1.Controllers
 {
-   //public class HomeController : Controller
+   public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "jean well done.0615fail";
+            ViewBag.Message = "jean well done.0619successfuly";
 
             return View();
         }
